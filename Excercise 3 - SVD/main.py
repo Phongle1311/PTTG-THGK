9b8c509b30dd8057022bc3e51c83c1edbf567e62
thach030103 <120116007+thach030103@users.noreply.github.com>
2023-11-22 13:42:38 +0700

    Update

--- Excercise 3 - SVD/main.py
+++ Excercise 3 - SVD/main.py
@@ -5,18 +5,22 @@ import matplotlib.pyplot as plt
 from tkinter import Canvas, Scrollbar, Tk, Button, filedialog, Scale, Listbox, HORIZONTAL
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+# Chuyển đổi giá trị của ảnh thành kiểu dữ liệu float trong khoảng từ 0 đến 1.
 def img2double(img):
     info = np.iinfo(img.dtype)
     return img.astype(float) / info.max
 
+# Thực hiện phân rã SVD cho một ma trận
 def svd(img, full_matrices=False):
     U, S, VT = np.linalg.svd(img, full_matrices=full_matrices)
     return U, np.diag(S), VT
 
+# Cập nhật ảnh nén khi giá trị của thanh trượt thay đổi
 def update_compressed_image(value):
     global RANK
     RANK = int(value)
 
+# Cập nhật và hiển thị ảnh nén khi giải phóng sự kiện của thanh trượt
 def release_update_compressed_image(value):
     global RANK, img, ax_compressed, canvas_compressed
 
@@ -34,12 +38,13 @@ def release_update_compressed_image(value):
 
     X_r = np.dstack((XR_r, XG_r, XB_r))
 
-    # Update the compressed image canvas
+    # Cập nhật canvas hình ảnh nén
     ax_compressed.clear()
     ax_compressed.imshow(X_r)
     ax_compressed.set_title(f'Compressed Image (Rank {RANK})')
     canvas_compressed.draw()
 
+# Cho phép người dùng chọn và thêm ảnh mới từ hộp thoại mở tệp
 def add_image():
     global img, canvas_original, RANK
 
@@ -48,9 +53,10 @@ def add_image():
         img = imread(file_path)
         img = img2double(img)
 
-        # Update the compressed image canvas
+        # Cập nhật canvas hình ảnh nén
         release_update_compressed_image(RANK)
 
+# Cập nhật ảnh nén khi người dùng chọn ảnh từ danh sách.
 def select_image(event):
     global img, RANK
 
@@ -61,40 +67,43 @@ def select_image(event):
         img = imread(img_path)
         img = img2double(img)
 
-        # Update the compressed image canvas
+        # Cập nhật canvas hình ảnh nén
         release_update_compressed_image(RANK)
+
+# Xử lý sự kiện đóng cửa sổ
 def on_closing():
     root.destroy()
 
-# Create the Tkinter window
+# Tạo cửa sổ chính cho ứng dụng
 root = Tk()
 root.title('Image Compression using SVD')
 
-# Create a button to add the selected image
+# Nút này mở hộp thoại mở tệp khi được nhấp để thêm ảnh.
 add_button = Button(root, text='Add Image', command=add_image)
 add_button.pack()
 
-# Create a slider for adjusting the rank
+# Cho phép người dùng chọn giá trị RANK để kiểm soát mức độ nén.
 RANK = 5
+
+# Thiết lập thanh slider
 slider = Scale(root, from_=1, to=100, orient='horizontal', label='Rank', command=update_compressed_image, resolution=1, length=300)
 slider.set(RANK)
 slider.pack()
 
-# Create a figure for the compressed image
+# Hiển thị ảnh nén sau khi áp dụng phương pháp SVD
 fig_compressed = plt.Figure(figsize=(4, 4))
 ax_compressed = fig_compressed.add_subplot(111)
 canvas_compressed = FigureCanvasTkAgg(fig_compressed, master=root)
 canvas_compressed.get_tk_widget().pack()
 
-# Set up the release event for the slider
+# Thiết lập sự kiện cho thanh slider
 slider.bind("<ButtonRelease-1>", lambda event: release_update_compressed_image(slider.get()))
 
-# Create a Listbox for displaying the list of images
+# Hiển thị tất cả các ảnh gốc trong thư mục 'img' và cho phép người dùng chọn ảnh để xem và nén
 listbox = Listbox(root)
 listbox.pack(side="bottom") 
 listbox.bind("<Button-1>", select_image)
 
-# Display all original images in the listbox
 for img_file in os.listdir('img'):
     img_path = os.path.join('img', img_file)
     original_img = imread(img_path)
@@ -108,8 +117,8 @@ for img_file in os.listdir('img'):
     canvas_original = FigureCanvasTkAgg(fig_original, master=listbox)
     canvas_original.get_tk_widget().pack(side='left')
 
-# Set up the window closing event
+# Khi người dùng đóng cửa sổ, chương trình sẽ kết thúc
 root.protocol("WM_DELETE_WINDOW", on_closing)
 
-# Start the Tkinter main loop
+# Mở cửa sổ chính
 root.mainloop()
