14f7c27ed863c61110f2496d246132ca8e766c64
tungluuai <77062218+tungluuai@users.noreply.github.com>
2023-11-22 18:27:19 +0700

    add some additional edits in PCA implementation

--- Excercise 4 - PCA/PCA.py
+++ Excercise 4 - PCA/PCA.py
@@ -1,9 +1,11 @@
 import numpy as np
 class PCA: 
     def __init__(self):
+        self.eigenvalues = None
         self.eigenvectors = None
         self.prj_matrix= None
         self.optimize = False
+        self.sum_of_var_explained = None
 
     def standardize(self, X):
         mu = np.mean(X, axis = 0) 
@@ -29,22 +31,41 @@ class PCA:
         P = U @ U.T 
         return P
 
+    def get_sum_of_variance_explained(self):
+        sum_of_var_explained = np.sum(self.eigenvalues)
+        return sum_of_var_explained
+    
     def fit(self, Xbar):
         if self.optimize:
             S = np.cov(Xbar.T)  
         else:
             S = np.cov(Xbar)
             S = S.astype(np.float64)
-        self.eigenvectors = self.find_eig_and_sort(S)[1]
+        self.eigenvalues, self.eigenvectors = self.find_eig_and_sort(S)
+        self.sum_of_var_explained = self.get_sum_of_variance_explained()
+    
+    def preserved_var_explained(self, ratio):
+        var_required = ratio * self.sum_of_var_explained
+        sum = 0 
+        num_components = 0
+        for value in self.eigenvalues:
+            num_components +=1
+            sum = sum + value
+            if sum > var_required:
+                break
+         
+        return num_components
 
-    def reconstruct_img(self, Xbar, num_components):
+    def reconstruct_img(self, Xbar, mu_Xbar, num_components = None, preserved = None):
+        if preserved is not None:
+            num_components = self.preserved_var_explained(preserved)
         U = self.eigenvectors[:, range(num_components)]
         self.prj_matrix = self.projection_matrix(U)
         if self.optimize:
-            reconstructed_img =  Xbar @ self.prj_matrix
+            reconstructed_img =  Xbar @ self.prj_matrix + mu_Xbar
         else:
             reconstructed_img = Xbar.T @self.prj_matrix
-            reconstructed_img = reconstructed_img.T
+            reconstructed_img = reconstructed_img.T + mu_Xbar
         return reconstructed_img
     
 
